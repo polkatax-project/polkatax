c93566f231a3acc7a4590e56aa548a4d266089a2
loanMaster <nicolai.fricker@gmx.ch>
2025-06-27 08:51:07 +0200

    fix: error logging

--- server/src/server/polkatax-server.ts
+++ server/src/server/polkatax-server.ts
@@ -45,7 +45,7 @@ export const polkataxServer = {
         );
         reply.status(error.statusCode).send(error.message);
       } else {
-        logger.warn(`Error: ${error.message}`, error);
+        logger.warn(`Error: ${error.message}`);
         if (error.stack) {
           logger.error(error.stack);
         }
