155cb9dba6d2394fe840f95d9a0dec8c07938877
loanMaster <nicolai.fricker@gmx.ch>
2025-09-01 20:25:34 +0200

    feat/parallel post processing

--- server/src/server/job-management/job.manager.ts
+++ server/src/server/job-management/job.manager.ts
@@ -1,15 +1,23 @@
 import { JobsService } from "./jobs.service";
 import * as subscanChains from "../../../res/gen/subscan-chains.json";
-import { Job } from "../../model/job";
-import { filter, firstValueFrom } from "rxjs";
+import { Job, JobId } from "../../model/job";
+import { filter, firstValueFrom, mergeMap, Subject } from "rxjs";
 import { determineNextJob } from "./determine-next-job";
 import { AwilixContainer } from "awilix";
 import { isEvmAddress } from "../data-aggregation/helper/is-evm-address";
-import { getBeginningLastYear } from "./get-beginning-last-year";
+import {
+  getBeginningLastYear,
+  getEndOfLastYear,
+} from "./get-beginning-last-year";
 import { logger } from "../logger/logger";
-import { JobConsumer } from "./job.consumer";
+import { JobProcessor } from "./job.processor";
+import { JobPostProcessor } from "./job.post-processor";
+
+const PARALLEL_POST_PROCESSING_JOBS = 2;
 
 export class JobManager {
+  private postProcessingQueue = new Subject<JobId>();
+
   constructor(
     private jobsService: JobsService,
     private DIContainer: AwilixContainer,
@@ -68,7 +76,14 @@ export class JobManager {
   }
 
   async start() {
+    this.startProcessing();
+    this.startPostProcessing();
+  }
+
+  private async startProcessing() {
     let previousWallet: string | undefined;
+
+    let job: Job | undefined;
 
     while (true) {
       try {
@@ -79,21 +94,86 @@ export class JobManager {
         const jobInfo = determineNextJob(jobs, previousWallet);
         if (!jobInfo) continue;
 
-        const job = await this.jobsService.fetchJob(
+        job = await this.jobsService.fetchJob(
           jobInfo.wallet,
           jobInfo.blockchain,
           jobInfo.currency,
         );
 
         previousWallet = job.wallet;
-        const jobConsumer: JobConsumer =
-          this.DIContainer.resolve("jobConsumer");
-        await jobConsumer.process(job);
+        const jobProcessor: JobProcessor =
+          this.DIContainer.resolve("jobProcessor");
+        job = await jobProcessor.process(job);
+        job.status = "post_processing";
+        await this.jobsService.setToPostProcessing(job);
+        this.enqueueForPostProcessing(job);
         const allPendingJobs = await this.jobsService.fetchAllPendingJobs();
         logger.info("Remaining pending jobs:" + allPendingJobs.length);
       } catch (error) {
+        this.handleError(error, job);
         logger.error(error);
       }
+    }
+  }
+
+  private async enqueueForPostProcessing(jobId: JobId) {
+    this.postProcessingQueue.next({
+      wallet: jobId.wallet,
+      blockchain: jobId.blockchain,
+      currency: jobId.currency,
+    });
+  }
+
+  private async startPostProcessing() {
+    this.postProcessingQueue
+      .pipe(
+        mergeMap(async (job) => {
+          logger.info(job, "Postprocessing job");
+          await this.doPostProcessing(job);
+          logger.info(job, "Finished postprocessing");
+        }, PARALLEL_POST_PROCESSING_JOBS),
+      )
+      .subscribe();
+  }
+
+  private async doPostProcessing(jobId: JobId) {
+    let job: Job | undefined;
+    try {
+      job = await this.jobsService.fetchJob(
+        jobId.wallet,
+        jobId.blockchain,
+        jobId.currency,
+      );
+
+      if (job.status !== "post_processing") {
+        return;
+      }
+
+      const jobPostProcessor: JobPostProcessor =
+        this.DIContainer.resolve("jobPostProcessor");
+      job = await jobPostProcessor.postProcess(job);
+      job.status = "done";
+      await this.jobsService.setDone(job, getEndOfLastYear());
+    } catch (error) {
+      this.handleError(error, job);
+      logger.error(error);
+    }
+  }
+
+  private async handleError(error: any, job: Job): Promise<void> {
+    try {
+      await this.jobsService.setError(
+        {
+          code: error?.statusCode ?? 500,
+          msg:
+            error?.message ??
+            `Unhandled error processing job: ${JSON.stringify(job)}`,
+        },
+        job,
+      );
+    } catch (nestedError) {
+      logger.error(nestedError, "JobConsumer: failed to set error state: ");
+      logger.error("Job details: ", job);
     }
   }
 }
